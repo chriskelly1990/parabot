76e3e9782a7745fea1c7cc571177d873247848bb
lulzs3c <official.lulzs3c@gmail.com>
2014-12-11 19:19:41 +0000

    updated
    attack npc update

--- Ipestcontrol/InBrawlers.java
+++ Ipestcontrol/InBrawlers.java
@@ -11,33 +11,30 @@ import org.rev317.min.api.wrappers.Tile;
  * Created by callum on 10/12/14.
  */
 public class InBrawlers implements Strategy {
+    private static Npc q;
 
 
 
     Area InBrawlers = new Area(new Tile(2671, 2585), new Tile(2671, 2598), new Tile(2683, 2598), new Tile(2683, 2585));
-    private Npc[] Brawler;
 
 
-    public boolean activate() { //if true continue to execute
 
-        if (InBrawlers.contains(Players.getMyPlayer().getLocation())) {
-
-            return true;
+    public boolean activate() {
+        for(Npc n : Npcs.getNearest(3776)){
+            q = n;
+            return n != null && !Players.getMyPlayer().isInCombat() && (InBrawlers.contains(Players.getMyPlayer().getLocation()));
         }
 
         return false;
     }
 
-
     public void execute() { //
         PestControl.status = "attacking npc's";
-        Brawler = Npcs.getNearest(3776, 3740, 3739);
-        if (Brawler.length > 0 &&!Players.getMyPlayer().isInCombat() && Players.getMyPlayer().getAnimation() == -1 && Brawler[0].distanceTo() < 8)  {
-            Brawler[0].interact(1);
-            Time.sleep(3500);
-
-        }
+        if(q != null){
+            q.interact(1); // Change to 1 if 0 doesn't work.
+            Time.sleep(1000);
 
 
     }
+}
 }
